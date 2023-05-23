56553ec0a4a033cacfe1719ca2359dd0c477de6f
Alirio DC <alirio.a@dcorp.dev>
2023-05-23 16:32:52 -0400

    functional

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,47 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import {useEffect, useState} from "react";
+import {ethers, providers} from "ethers";
+import BAUL_ABI from "@/utils/Baul.abi.json"
+import NFTABI from "@/utils/NFT.abi.json"
 
 const inter = Inter({ subsets: ['latin'] })
 
+const RPC = process.env.NEXT_PUBLIC_RPC_URL as string;
+const RPC_PROVIDER = new providers.JsonRpcProvider(RPC);
+const NFT_ADDRESS = process.env.NEXT_PUBLIC_NFT_ADDRESS as string;
+const BAUL_ADDRESS = process.env.NEXT_PUBLIC_BAUL_ADDRESS as string;
+
+const NFTContract = new ethers.Contract(
+    NFT_ADDRESS,
+    NFTABI,
+    RPC_PROVIDER
+);
+const BAULContract = new ethers.Contract(
+    BAUL_ADDRESS,
+    BAUL_ABI,
+    RPC_PROVIDER
+);
 export default function Home() {
+
+  const [busd, setBusd] = useState(0);
+  const [usdt, setUsdt] = useState(0);
+  const [nft, setNft] = useState(0);
+  useEffect(() => {
+
+    const init = async () => {
+      const usdt = await BAULContract.balanceOfUSDT();
+      const busd = await BAULContract.balanceOfBUSD();
+      const nft = await NFTContract.totalSupply();
+      setNft(Number(nft));
+      setBusd(parseInt(ethers.utils.formatUnits(busd,"ether")));
+      setUsdt(parseInt(ethers.utils.formatUnits(usdt,"ether")));
+    }
+      init();
+  }, []);
+
+
   return (
     <>
       <Head>
@@ -18,7 +55,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Contratos de Plusvalica&nbsp;<br/>
-            <code className={styles.code}>NFT: 0xBa84f596Dc68EE3c64CD7c037469E82aa1779C3c <br/> Baul: 0x358d019b5E370bfe69FB452a7A6773ec77540edC </code>
+            <code className={styles.code}>NFT: {NFT_ADDRESS} <br/> Baul: {BAUL_ADDRESS} </code>
           </p>
           <div>
             <a
@@ -41,9 +78,9 @@ export default function Home() {
         <div className={styles.center}>
           <div className={styles.wrapper}>
             <h3>Balances</h3>
-            <h4 className={styles.headlines}>Cantidad de nfts: <span> 46 </span></h4>
-            <h4 className={styles.headlines}>BUSD en baul:  <span> 400mil $</span></h4>
-            <h4 className={styles.headlines}>USDT en baul:  <span> 400mil $</span></h4>
+            <h4 className={styles.headlines}>Cantidad de nfts: <span> {nft} </span></h4>
+            <h4 className={styles.headlines}>BUSD en baul:  <span> {busd} $</span></h4>
+            <h4 className={styles.headlines}>USDT en baul:  <span> {usdt} $</span></h4>
           </div>
         </div>
 
